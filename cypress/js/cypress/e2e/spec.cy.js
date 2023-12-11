5cd50a78114902ecd10dad786ff044512132be6c
Lara Bischoff <lbischoff@proficom.de>
2023-12-11 16:48:57 +0100

    Update xpath selectors

--- cypress/js/cypress/e2e/spec.cy.js
+++ cypress/js/cypress/e2e/spec.cy.js
@@ -29,73 +29,74 @@ describe('Advantage Online Shopping', () => {
     beforeTestStartTS = Date.now()
 
     // Check that cart is empty
-    cy.xpath('//*[@id="menuCart"]').click()
-    cy.xpath('//*[@id="shoppingCart"]/div/label').contains("Your shopping cart is empty")
+    cy.xpath('//a[@id="shoppingCartLink"]').click()
+    cy.xpath('//div[@id="shoppingCart"]').contains("Your shopping cart is empty")
     if (screenshot) {cy.screenshot({ capture: 'viewport'})}
-    cy.xpath('//*[@id="shoppingCart"]/div/a').click()
+    cy.xpath('//div[@id="shoppingCart"]//a[@translate="CONTINUE_SHOPPING"]').click()
+    
     // Category "Mice"
-    cy.xpath('//*[@id="miceImg"]').click() // select category
-    cy.xpath("//div[@id='mobileSlide']//ul/li[2]").click() // open filter: scroller type
-    cy.xpath("//div[@id='mobileSlide']//ul/li[2]//label[./text()='Scroll Ball']/../input").click({force: true}) // select scroll bar
-    cy.xpath("//div[@id='mobileSlide']//ul/li[2]//label[./text()='Scroll Ring']/../input").click({force: true}) // select scroll ring
+    cy.xpath('//div[@id="miceImg"]').click() // select category
+    cy.xpath('//h4[contains(text(),"SCROLLER TYPE")]').click() // open filter: scroller type
+    cy.xpath('//label[./text()="Scroll Ball"]/../input').click() // select scroll ball
+    cy.xpath('//label[./text()="Scroll Ring"]/../input').click() // select scroll ring
 
     cy.xpath('//div[@class="cell categoryRight"]/ul/li').should('have.length', 2)
 
     if (screenshot) {cy.screenshot({ capture: 'viewport'})}
-    cy.xpath('//*[@id="26"]').click() // select mice
-    cy.xpath('//*[@id="rabbit"]').click({ multiple: true }) // select color red
-    cy.xpath('//*[@id="productProperties"]/div[4]/button').click() // add to cart
+    cy.xpath('//a[./text()="Kensington Orbit 72352 Trackball"]').click() // select mice
+    cy.xpath('//span[@title="RED"]').click() // select color red
+    cy.xpath('//button[@name="save_to_cart"]').click() // add to cart
     
     // Back to home-screen
-    cy.xpath('/html/body/div[3]/nav/a[1]').click()
+    cy.xpath('//div[@class="logo"]').click()
 
     // Popular items
-    cy.xpath('//*[@id="popular_items"]/h3').scrollIntoView() // scroll to popular items
+    cy.xpath('//article[@id="popular_items"]').scrollIntoView() // scroll to popular items
     if (screenshot) {cy.screenshot({ capture: 'viewport'})}
-    cy.xpath('//*[@id="details_21"]').click({force: true}) // select speaker
-    cy.xpath('//*[@id="productProperties"]/div[2]/e-sec-plus-minus/div/div[3]').click({force: true}) // quantity 2
-    cy.xpath('//*[@id="productProperties"]/div[4]/button').click({force: true}) // add to cart
+    cy.xpath('//div[./p/text()="HP ROAR PLUS WIRELESS SPEAKER"]/a').click({force: true}) // select speaker
+    cy.xpath('//div[@class="plus"]').click({force: true}) // quantity 2
+    cy.xpath('//button[@name="save_to_cart"]').click({force: true}) // add to cart
 
     // Show shopping cart
-    cy.xpath('//*[@id="shoppingCartLink"]').click() // click on shopping cart
+    cy.xpath('//a[@id="shoppingCartLink"]').click() // click on shopping cart
     cy.contains('HP ROAR PLUS WIRELESS SPEAKER').should('exist')
     cy.contains('2').should('exist')
     cy.contains('KENSINGTON ORBIT 72352 TRACKBALL').should('exist')
     cy.contains('1').should('exist')
-    cy.xpath('//*[@id="shoppingCart"]/table/tfoot/tr[1]/td[2]/span[2]').contains("$399.97")
+    cy.xpath('//td[./span[1]/text()="TOTAL:"]/span[2]').contains("$399.97")
     if (screenshot) {cy.screenshot({ capture: 'viewport'})}
     cy.xpath('//*[@id="checkOutButton"]').click() // Checkout
 
     // Create new user
     cy.xpath('//*[contains(@id, "registration_btn")]').click()
     var name = generateUsername(username)
-    cy.xpath('//*[@id="formCover"]/div[1]/div[1]/sec-view[1]/div/input').type(name)
-    if (screenshot) {cy.xpath('//*[@id="formCover"]/div[4]/span').screenshot()}
-    cy.xpath('//*[@id="formCover"]/div[1]/div[1]/sec-view[2]/div/input').click({force: true}).type(mail)
-    cy.xpath('//*[@id="formCover"]/div[1]/div[2]/sec-view[1]/div/input').type(password)
-    cy.xpath('//*[@id="formCover"]/div[1]/div[2]/sec-view[2]/div/input').click({force: true}).type(password)
-    cy.xpath('//*[@id="formCover"]/div[2]/div[1]/sec-view[1]/div/input').type(firstName)
-    cy.xpath('//*[@id="formCover"]/div[2]/div[1]/sec-view[2]/div/input').click({force: true}).type(lastName)
-    cy.xpath('//*[@id="formCover"]/div[3]/div[1]/sec-view[1]/div/select').select(country)
-    cy.xpath('//*[@id="formCover"]/div[3]/div[1]/sec-view[2]/div/input').type(city)
-    cy.xpath('//*[@id="formCover"]/div[4]/input').click() // no advertisement
-    cy.xpath('//*[@id="formCover"]/sec-view/div/input').click() // agree conditions
+    cy.xpath('//input[@name="usernameRegisterPage"]').type(name)
+    if (screenshot) {cy.xpath('//input[@name="allowOffersPromotion"]/../input').screenshot()}
+    cy.xpath('//input[@name="emailRegisterPage"]').click({force: true}).type(mail)
+    cy.xpath('//input[@name="passwordRegisterPage"]').type(password)
+    cy.xpath('//input[@name="confirm_passwordRegisterPage"]').click({force: true}).type(password)
+    cy.xpath('//input[@name="first_nameRegisterPage"]').type(firstName)
+    cy.xpath('//input[@name="last_nameRegisterPage"]').click({force: true}).type(lastName)
+    cy.xpath('//select[@name="countryListboxRegisterPage"]').select(country)
+    cy.xpath('//input[@name="cityRegisterPage"]').type(city)
+    cy.xpath('//input[@name="allowOffersPromotion"]').click() // no advertisement
+    cy.xpath('//input[@name="i_agree"]').click() // agree conditions
     cy.xpath('//*[contains(@id, "register_btn")]').click() // Register
 
     // Check shipping details
-    cy.xpath('//*[@id="userDetails"]/div[1]/label').contains(firstName + " " + lastName)
-    cy.xpath('//*[@id="userDetails"]/div[2]/label[2]').contains(city)
-    cy.xpath('//*[@id="userDetails"]/div[2]/label[3]').contains(country)
+    cy.xpath('//div[./img/@alt="user"]/label').should('be.visible').contains(firstName + " " + lastName)
+    cy.xpath('//div[./div/@class="iconCss iconHome"]').contains(city)
+    cy.xpath('//div[./div/@class="iconCss iconHome"]').contains(country)
     cy.xpath('//*[@id="next_btn"]').click({multiple: true, force: true}) // click Next
 
     // Payment Master Credit
-    cy.xpath('//*[@id="paymentMethod"]/div/div[1]/div[2]/input').click()
-    cy.xpath('//*[@id="paymentMethod"]/div/div[4]/sec-form/div[2]/div/sec-view[1]/div/select').select(CCmonth)
-    cy.xpath('//*[@id="paymentMethod"]/div/div[4]/sec-form/div[2]/div/sec-view[2]/div/select').select(CCyear)
-    cy.xpath('//*[@id="paymentMethod"]/div/div[4]/sec-form/div[2]/sec-view/div/input').type(CCholder)
-    cy.xpath('//*[@id="paymentMethod"]/div/div[4]/sec-form/div[1]/sec-view[2]/div/input').type(CVS,{force:true})
-    cy.xpath('//*[@id="creditCard"]').type(CCnumber,{force:true})
-    cy.xpath('//*[@id="pay_now_btn_ManualPayment"]').click() // click Pay Now
+    cy.xpath('//input[@name="cvv_number"]').type(CVS,{force:true})
+    cy.xpath('//input[@name="masterCredit"]').click()
+    cy.xpath('//select[@name="mmListbox"]').select(CCmonth)
+    cy.xpath('//select[@name="yyyyListbox"]').select(CCyear)
+    cy.xpath('//input[@name="cardholder_name"]').type(CCholder)
+    cy.xpath('//input[@id="creditCard"]').type(CCnumber,{force:true})
+    cy.xpath('//button[@id="pay_now_btn_ManualPayment"]').click() // click Pay Now
 
     // Tracking number and Order number
     //cy.xpath("//div[@class!='ng-hide' and ./div/@id='orderPaymentSuccess']").should('be.visible')
